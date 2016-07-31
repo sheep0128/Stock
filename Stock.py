9a600f25ec991c32f8c51c06e91f24302087c4b6
sheep0128 <sheep0128@gmail.com>
2016-08-01 01:33:58 +0900

    stock candle graph

--- Stock.py
+++ Stock.py
@@ -1,5 +1,7 @@
 import pandas_datareader.data as web
 import datetime
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.finance as matfin
 import matplotlib.ticker as ticker
@@ -10,27 +12,84 @@ def main():
     #end = datetime.datetime(2016, 3, 31)
     #skhynix = web.DataReader("078930.KS", "yahoo", start, end)
 
-    stockInfo = StockReader.getStockItem("040350").tail(10)
+    stockInfo = StockReader.getStockItem("026260").tail(30)
 
-    print(stockInfo.index[0])
-    print(stockInfo.dtypes)
+    stockInfo['MA5'] = stockInfo['Adj Close'].rolling(window=5).mean()
+    stockInfo['MA20'] = stockInfo['Adj Close'].rolling(window=20).mean()
+    stockInfo['MA60'] = stockInfo['Adj Close'].rolling(window=60).mean()
+    stockInfo['MA120'] = stockInfo['Adj Close'].rolling(window=120).mean()
+    #print(stockInfo)
     fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(111)
+    ax = fig.add_subplot(311)
 
     day_list = []
     name_list = []
-    for i, day in enumerate(stockInfo.index):
-        if day.dayofweek == 0:
-            day_list.append(i)
-            name_list.append(day.strftime('%Y-%m-%d') + '(Mon)')
 
+    for i, day in enumerate(stockInfo.index):
+        #if day.dayofweek == 0:
+            day_list.append(i)
+            name_list.append(day.strftime('%d'))
+            #name_list.append(day.strftime('%Y-%m-%d') + '(Mon)')
+        #else:
+        #    print(day.weekday())
+
+
+    print(day_list)
+    print(name_list)
+
+    ax.text(.5, .9, 'centered title',
+            horizontalalignment='center',
+            transform=ax.transAxes)
     ax.xaxis.set_major_locator(ticker.FixedLocator(day_list))
     ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
 
     matfin.candlestick2_ohlc(ax, stockInfo['Open'], stockInfo['High'], stockInfo['Low'], stockInfo['Close'], width=0.5,
                              colorup='r', colordown='b')
     plt.grid()
+    print(stockInfo['Volume'])
+
+    ax = fig.add_subplot(312)
+    ax.bar(stockInfo['Volume'].index, stockInfo['Volume'], width=0.8)
+    plt.grid()
+
+
+    plt.figure(1)
+    plt.subplot(313)
+    plt.plot(stockInfo.index, stockInfo['MA5'], label="MA5")
+    plt.plot(stockInfo.index, stockInfo['MA20'], label="MA20")
+    plt.plot(stockInfo.index, stockInfo['MA60'], label="MA60")
+    #plt.plot(stockInfo.index, stockInfo['MA120'], label="MA120")
+    plt.legend(loc='best')
+
+    plt.grid()
     plt.show()
 
 if __name__ == "__main__":
     main()
+
+# import pandas_datareader.data as web
+# import datetime
+# import matplotlib.pyplot as plt
+# import matplotlib.finance as matfin
+# import matplotlib.ticker as ticker
+#
+# start = datetime.datetime(2016, 3, 1)
+# end = datetime.datetime(2016, 3, 31)
+# skhynix = web.DataReader("000660.KS", "yahoo", start, end)
+# #skhynix = skhynix[skhynix['Volume'] > 0]
+#
+# fig = plt.figure(figsize=(12, 8))
+# ax = fig.add_subplot(111)
+#
+# day_list = range(len(skhynix))
+# name_list = []
+# for day in skhynix.index:
+#     name_list.append(day.strftime('%d'))
+#
+# print(day_list)
+# print(name_list)
+# ax.xaxis.set_major_locator(ticker.FixedLocator(day_list))
+# ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
+#
+# matfin.candlestick2_ohlc(ax, skhynix['Open'], skhynix['High'], skhynix['Low'], skhynix['Close'], width=0.5, colorup='r', colordown='b')
+# plt.show()
--- StockReader.py
+++ StockReader.py
@@ -17,10 +17,23 @@ def getStockItem(stockItem):
     mp = maxPage[0].find_all("td", class_="pgRR")
     mpNum = int(mp[0].a.get('href')[-3:])
 
-    stockInfo = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+    date_list = []
+    #stockInfo = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+    stockInfo = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+    # first = datetime.date(2016, 1, 1)
+    # date_list.append(first)
+    # stock = Series(
+    #     [float(0),
+    #      float(0),
+    #      float(0),
+    #      float(0),
+    #      int(0),
+    #      float(0)],
+    #     index=['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+    # stockInfo = stockInfo.append(stock, ignore_index=True)
 
     #for page in range(1, mpNum + 1):
-    for page in range(1, 3):
+    for page in range(1, 6):
         print(str(page))
         url = 'http://finance.naver.com/item/sise_day.nhn?code=' + stockItem + '&page=' + str(page)
         html = urlopen(url)
@@ -30,7 +43,6 @@ def getStockItem(stockItem):
 
         # if ((page % 1) == 0):
         # time.sleep(1.50)
-
         for i in range(1, len(srlists) - 1):
             if (srlists[i].span != isCheckNone):
                 srlists[i].td.text
@@ -38,19 +50,30 @@ def getStockItem(stockItem):
                 #    [srlists[i].find_all("td", align="center")[0].text, srlists[i].find_all("td", class_="num")[2].text, srlists[i].find_all("td", class_="num")[3].text,
                 #     srlists[i].find_all("td", class_="num")[4].text, srlists[i].find_all("td", class_="num")[0].text,
                 #     srlists[i].find_all("td", class_="num")[5].text, srlists[i].find_all("td", class_="num")[0].text]]
-                stock = Series(
-                    [srlists[i].find_all("td", align="center")[0].text, float(srlists[i].find_all("td", class_="num")[2].text.replace(',','')),
-                     float(srlists[i].find_all("td", class_="num")[3].text.replace(',', '')),
-                     float(srlists[i].find_all("td", class_="num")[4].text.replace(',','')), float(srlists[i].find_all("td", class_="num")[0].text.replace(',','')),
-                     int(srlists[i].find_all("td", class_="num")[5].text.replace(',','')), float(srlists[i].find_all("td", class_="num")[0].text.replace(',',''))],
-                    index=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+                date_list.append(srlists[i].find_all("td", align="center")[0].text);
+
+                if (int(srlists[i].find_all("td", class_="num")[5].text.replace(',', '')) == 0):
+                    stock = Series(
+                        [float(srlists[i].find_all("td", class_="num")[0].text.replace(',', '')),
+                         float(srlists[i].find_all("td", class_="num")[0].text.replace(',', '')),
+                         float(srlists[i].find_all("td", class_="num")[0].text.replace(',', '')),
+                         float(srlists[i].find_all("td", class_="num")[0].text.replace(',', '')),
+                         int(srlists[i].find_all("td", class_="num")[5].text.replace(',', '')),
+                         float(srlists[i].find_all("td", class_="num")[0].text.replace(',', ''))],
+                        index=['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+                else:
+                    stock = Series(
+                        [float(srlists[i].find_all("td", class_="num")[2].text.replace(',', '')),
+                         float(srlists[i].find_all("td", class_="num")[3].text.replace(',', '')),
+                         float(srlists[i].find_all("td", class_="num")[4].text.replace(',', '')), float(srlists[i].find_all("td", class_="num")[0].text.replace(',', '')),
+                         int(srlists[i].find_all("td", class_="num")[5].text.replace(',', '')), float(srlists[i].find_all("td", class_="num")[0].text.replace(',', ''))],
+                        index=['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
                 stockInfo = stockInfo.append(stock, ignore_index=True)
 
 
                 # print(srlists[i].find_all("td", align="center")[0].text,
                 #       srlists[i].find_all("td", class_="num")[0].text, srlists[i].find_all("td", class_="num")[5].text)
 
-    stockInfo.set_index(pd.DatetimeIndex(stockInfo['Date']), inplace=True)
+    stockInfo.set_index(pd.DatetimeIndex(date_list), inplace=True)
     stockInfo.sort_index(inplace=True, ascending=True)
-    print(stockInfo)
     return stockInfo
